d97514b54653a23fa35986445d0b68ebfa241902
tmxk2012197 <macheng917@yahoo.com>
2016-12-11 15:12:56 -0500

    heatmap- tooltip

--- heatmap/P2PHeatmap.js
+++ heatmap/P2PHeatmap.js
@@ -1,12 +1,12 @@
 var margin = {top: 120, right: 120, bottom: 20, left: 120};
 var outerWidth = 860;
-var outerHeight = 760;
+var outerHeight = 800;
 var width = outerWidth - margin.right - margin.left;
 var height = outerHeight - margin.top - margin.bottom;
 var monthSelected = 1;
 var originData = [];
 
-var itemSize = 12;
+var itemSize = 13;
 var cellSize = itemSize - 1;
 var legendElementSize = 40;
 var lengendElementY = height;
@@ -19,7 +19,10 @@ var svg = d3.select(".heatmap").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var tip = d3.tip().attr("class", "d3-tip").html(function (d) {
-    return "volume:" + d.Volume;
+    //return "<div><span style='color:red'>Volume:</span><span> " + d.Volume + "</span></div>";
+    return "<div><span style='color:red'>NameX:</span><span> " + d.Name1 + "</span></div>" +
+        "<div><span style='color:red'>NameY:</span><span> " + d.Name2 + "</span></div>" +
+        "<div><span style='color:red'>Volume:</span><span> " + d.Volume + "</span></div>";
 });
 svg.call(tip);
 
@@ -34,11 +37,19 @@ function setMonth(newMonth) {
 }
 
 function render(result) {
-    var resultNameChanged = changeName(result).filter(function (d) {
+    /*var result1 = changeName(result).filter(function (d) {
+        return d.month == monthSelected;
+    });*/
+
+    var result1 = result.filter(function (d) {
         return d.month == monthSelected;
     });
-    //var resultNameChanged = changeName(result);
-    var data = resultNameChanged.map(function (d) {
+    result1.forEach(function (d) {
+        d.name1 = nameTrans(d.name1);
+        d.name2 = nameTrans(d.name2);
+    });
+
+    var data = result1.map(function (d) {
         var dataset = [];
         dataset.Name1 = d.name1;
         dataset.Name2 = d.name2;
@@ -46,6 +57,13 @@ function render(result) {
         dataset.Month = d.month;
         return dataset;
     });
+    /*var resultNameChanged = result.filter(function (d) {
+        return d.month == monthSelected;
+    });
+    resultNameChanged.forEach(function (d) {
+        d.name1 = nameTrans(d.name1);
+        d.name2 = nameTrans(d.name2);
+    });*/
     createHeatmap(data);
 }
 
@@ -54,20 +72,10 @@ function createHeatmap(data) {
         return d.Name1;
     })).values();
 
-    var yElementsArray = xElementsArray;
-   /* var yElementsArray = d3.set(data.map(function (d) {
+    //var yElementsArray = xElementsArray;
+   var yElementsArray = d3.set(data.map(function (d) {
         return d.Name2;
-    })).values().sort(function (a, b) {
-        var nameA = a.toUpperCase();
-        var nameB = b.toUpperCase();
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }
-        return 0;
-    });*/
+    })).values();
 
     var xScale = d3.scale.ordinal().domain(xElementsArray).rangeBands([0, xElementsArray.length * itemSize]);
     var yScale = d3.scale.ordinal().domain(yElementsArray).rangeBands([0, yElementsArray.length * itemSize]);
@@ -84,10 +92,7 @@ function createHeatmap(data) {
         .orient("left");
 
     var colorScale = d3.scale.quantile()
-        .range(colors).domain([0, 10, 170]);
-        /*.domain([0, 9, d3.max(data, function (d) {
-            return d.Volume;
-        })]);*/
+        .range(colors).domain([0, 9, 167]);
 
     var cells = svg.selectAll("rect").data(data);
     cells.enter().append("g").append("rect")
@@ -113,11 +118,9 @@ function createHeatmap(data) {
         .call(xAxis)
         .selectAll("text")
         .attr("font-weight", "normal")
-        .attr("transform", function (d) {
-            return "rotate(-90)";
-        })
+        .attr("transform", "rotate(-90)")
         .attr("dx", ".8em")
-        .attr("dy", ".5em")
+        .attr("dy", "1.2em")
         .style("text-anchor", "start");
 
     svg.append("g")
