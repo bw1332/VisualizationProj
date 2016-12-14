ecae9ce4db1ebb8874ab40740c7d41532dc34939
Fancylynn <ys61@nyu.edu>
2016-12-14 14:23:48 -0500

    Add files via upload

--- Vis Project/barchart.js
+++ Vis Project/barchart.js
@@ -1,6 +1,6 @@
 d3.json("bars.json", function(json) {
 	var chartwidth= 500;
-    var chartheight = 500;
+    var chartheight = 600;
     
     var svg = d3.select("#chart")
     .append("svg")
@@ -19,7 +19,7 @@ d3.json("bars.json", function(json) {
     console.log(chartheight);
     console.log(data.length);
     
-    var dx = chartwidth / max_n;
+    var dx = (chartwidth -300) / max_n;
     
     var dy = chartheight / data.length;
 
@@ -30,21 +30,31 @@ d3.json("bars.json", function(json) {
         .append("rect")
         .attr("class", function(d, i) {return "bar " + d.name;})
         .attr("x", function(d, i) {return 0;})
-        .attr("y", function(d, i) {return dy*i;})
+        .attr("y", function(d, i) {return (dy+5)*i;})
         .attr("width", function(d, i) {return dx*d.volume})
         .attr("height", dy)
-        .attr("fill", "lightgreen")
-        .style("stroke", "black");
+        .attr("fill", "lightgreen");
 
     // labels
-    var text = svg.selectAll("text")
+    var text = svg.selectAll(".text1")
         .data(data)
         .enter()
         .append("text")
-        .attr("class", function(d, i) {return "label " + d.label;})
+//        .attr("class", function(d, i) {return "label " + d.label;})
         .attr("x", 5)
-        .attr("y", function(d, i) {return dy*i + 15;})
-        .text( function(d) {return d.name + "          (" + d.volume  + ")";})
-        .attr("font-size", "15px")
+        .attr("y", function(d, i) {return (dy+5)*i + 20;})
+        .text( function(d) {return d.name;})
+        .style("font-size", "15px")
+        .style("font-weight", "bold");
+//});
+    var text1 = svg.selectAll(".text2")
+        .data(data)
+        .enter()
+        .append("text")
+//        .attr("class", function(d, i) {return "label " + d.label;})
+        .attr("x", 400)
+        .attr("y", function(d, i) {return (dy+5)*i + 20;})
+        .text( function(d) {return d.volume;})
+        .style({"font-size": "15px", "fill":"#777"})
         .style("font-weight", "bold");
                 });
