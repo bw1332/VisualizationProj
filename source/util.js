78bcf8fa1d8cd9b0042fadaf6b246842a367bbd1
Bo Wang <bw1332@nyu.edu>
2016-12-15 20:23:56 -0500

    util

--- source/util.js
+++ source/util.js
@@ -44,8 +44,91 @@ function getMonthAndAmount(data){
 
 // objs is an array of json objs for given month
 // this function usually uses getMonthSource() as input
-function getHeatMapFromTo(objs){
+// return [{from,to, num}];
+function getHeatMapDataFromTo(objs){
   return getJsonArrWith(objs, ["from","to","num"]);
 }
 
-//
+// objs is an array of json objs for given month
+// this function usually uses getMonthSource() as input
+// return [{from,to,num}] but in from to have same meanning in this case, num is the total num of sending and reveiving
+function getHeatMapDataTotal(objs){
+    var fromToList = getHeatMapDataFromTo(objs);
+    fromToList.map(function(one){
+      fromToList.map(function(other){
+          if (one.from == other.to && one.to == other.from) {
+              one.num = one.num + other.num;
+          }
+      });
+    });
+    return fromToList;
+}
+
+// from _SOURCE
+// return a array of email sorted by total nums
+function findEmailList(data){
+    var all = new Object();
+    for (var i = 1; i <= 12; i++) {
+       data[i].map(function(e){
+           var to = e.to;
+           var from = e.from;
+           if (all.hasOwnProperty(to)){
+               var num = all[to];
+               num = parseInt(num) + 1;
+               all[to] = num;
+           } else {
+               all[to] = 1;
+           }
+           if (all.hasOwnProperty(from)){
+               var num = all[from];
+               num = parseInt(num) + 1;
+               all[from] = num;
+           } else {
+               all[from] = 1;
+           }
+       });
+    }
+    var list = [];
+    for (var att in all) {
+        list.push([att,all[att]]);
+    }
+    list.sort(function(a, b){
+        return (b[1]) - (a[1]);
+    });
+    
+    var res = [];
+    list.map(function(x){
+        res.push(x[0]);
+    })
+    return res;
+}
+
+// data is array from given month data, from email to email
+// 
+function getBarData(data, from, to){
+    var res = []
+    data.map(function(o){
+        if (o.from == from && o.to == to){
+            for (var i = 0; i < o.topics.length; i++) {
+                var one = new Object();
+                one.topic = o.topics[i];
+                one.times = o.times[i];
+                res.push(one);
+            }
+        }
+    });
+    return res;
+}
+
+// input email addr
+// return the name
+function cutEmail(email){
+    var end = email.indexOf("@");
+    if (end > 0) {
+        return email.substring(0,end).replace("."," ");
+    } 
+    return  email;
+}
+
+
+
