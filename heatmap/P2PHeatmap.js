feed9aa7fa26342161f7262c88711e8de77f6a8d
tmxk2012197 <macheng917@yahoo.com>
2016-12-16 14:40:31 -0500

    new tooltip

--- heatmap/P2PHeatmap.js
+++ heatmap/P2PHeatmap.js
@@ -9,7 +9,7 @@ var heatmapCellSize = heatmapItemSize - 1;
 //var legendElementSize = 60;
 //var lengendElementY = heatmapHeight;
 var colors = colorbrewer.Blues[9];
-var colorScale = d3.scale.quantile().range(colors).domain([0, 9, 167]);
+var heatmapColorScale = d3.scale.quantile().range(colors).domain([0, 9, 167]);
 
 var originData = [];
 var datasets = ["https://rawgit.com/bw1332/VisualizationProj/master/source/fii.json"];
@@ -25,13 +25,10 @@ var heatmapSvg = d3.select(".heatmap").append("svg")
     .append("g")
     .attr("transform", "translate(" + heatmapMargin.left + "," + heatmapMargin.top + ")");
 
-var tip = d3.tip().attr("class", "d3-tip").html(function (d) {
-    //return "<div><span style='color:red'>Volume:</span><span> " + d.Volume + "</span></div>";
-    return "<div><span style='color:red'>NameX:</span><span> " + d.Name1 + "</span></div>" +
-        "<div><span style='color:red'>NameY:</span><span> " + d.Name2 + "</span></div>" +
-        "<div><span style='color:red'>Volume:</span><span> " + d.Volume + "</span></div>";
-});
-heatmapSvg.call(tip);
+/*var tip = d3.tip().attr("class", "d3-tip").html(function (d) {
+    return d.Name1 + "<br/>" +  d.Name2 + "<br/>" + d.Volume ;
+});*/
+//heatmapSvg.call(tip);
 
 generateHeatmapChart(datasets[0]);
 
@@ -51,16 +48,9 @@ datasetPickerTotal.enter()
     .attr("type", "button")
     .attr("class", "dataset-button-total")
     .on("click", function(d) {
-        generateHeatmapChartTotal(d);
+        generateHeatmapChart(d);
         pickerSelector = 1;
     });
-
-function generateHeatmapChartTotal(jsonFile) {
-    d3.json(jsonFile, function (error, result) {
-        originData = result;
-        renderTotal(getMonthSource(result, monthSelected));
-    });
-}
 
 function generateHeatmapChart(jsonFile) {
     d3.json(jsonFile, function (error, result) {
@@ -69,15 +59,23 @@ function generateHeatmapChart(jsonFile) {
     });
 }
 
+/*function generateHeatmapChartFrom(jsonFile) {
+    d3.json(jsonFile, function (error, result) {
+        originData = result;
+        render(getMonthSource(result, monthSelected));
+    });
+}*/
+
 function setMonth(newMonth) {
     monthSelected = newMonth;
-    pickerSelector == 0 ? render(getMonthSource(originData, monthSelected)) :  renderTotal(getMonthSource(originData, monthSelected));
+    render(getMonthSource(originData, monthSelected));
+    //pickerSelector == 0 ? render(getMonthSource(originData, monthSelected)) :  renderTotal(getMonthSource(originData, monthSelected));
 }
 
-function renderTotal(result) {
+function render(result) {
    // var result1 = getMonthSource(result, monthSelected);
-    var result2 = getHeatMapDataTotal(result);
-    var data = result2.map(function (d) {
+    var resultSelected = pickerSelector == 0 ? result : getHeatMapDataTotal(result);
+    var data = resultSelected.map(function (d) {
         var dataset = [];
         dataset.Name1 = d.from;
         dataset.Name2 = d.to;
@@ -88,16 +86,7 @@ function renderTotal(result) {
     createHeatmap(data, originData);
 }
 
-function render(result) {
-    /*var result1 = result.filter(function (d) {
-        return d.month == monthSelected;
-    });*/
-    /*result1.forEach(function (d) {
-        d.name1 = nameTrans(d.name1);
-        d.name2 = nameTrans(d.name2);
-    });*/
-   // var result1 = getMonthSource(result, monthSelected);
-    //console.log(result1[0]);
+/*function renderFrom(result) {
     var data = result.map(function (d) {
         var dataset = [];
         dataset.Name1 = d.from;
@@ -107,18 +96,13 @@ function render(result) {
         return dataset;
     });
     createHeatmap(data, originData);
-}
+}*/
 
-function createHeatmap(data, data1) {
+function createHeatmap(data, allData) {
     /*var xElementsArray = d3.set(data.map(function (d) {
         return cutEmail(d.Name1);
     })).values();*/
-    var xElementsArray = findEmailList(data1);
-    /*console.log(xElementsArray.indexOf(data[0].Name1));
-    console.log(data[0].Name1);*/
-    /*xElementsArray.forEach(function (d) {
-        console.log(xElementsArray.indexOf(d));
-    });*/
+    var xElementsArray = findEmailList(allData);
     var yElementsArray = xElementsArray;
    /*var yElementsArray = d3.set(data.map(function (d) {
         return cutEmail(d.Name2);
@@ -137,7 +121,7 @@ function createHeatmap(data, data1) {
             return nameTrans(d);
         })
         .orient("left");
-    //heatmapSvg.selectAll("rect").remove();
+
     var cells = heatmapSvg.selectAll("rect").data(data);
     cells.enter().append("g").append("rect")
         .attr("class", "cell")
@@ -154,40 +138,52 @@ function createHeatmap(data, data1) {
         .attr("class", function(d){return "cell cell-border x" + xElementsArray.indexOf(d.Name1) + " y" + yElementsArray.indexOf(d.Name2)})
         //.on("click", tip.show)
         .on("click", function (d) {
-            console.log(d.Name1);
-            console.log(d.Name2);
+            //console.log(d.Name1);
+            //console.log(d.Name2);
             valuesForBarChart[0] = d.Name1;
             valuesForBarChart[1] = d.Name2;
             console.log(valuesForBarChart[0]);
             console.log(valuesForBarChart[1]);
         })
-        .on("mouseover", function (d) {
+        .on("mouseenter", function (d) {
             d3.select(this).classed("cell-hover",true);
-            d3.selectAll(".xLabel").classed("text-highlight",function(r){ return r==d.Name1;});
-            d3.selectAll(".yLabel").classed("text-highlight",function(c){ return c==d.Name2;});
-            tip.show(d)
+            d3.selectAll(".xLabel").classed("text-highlight",function(r){ return r == d.Name1;});
+            d3.selectAll(".yLabel").classed("text-highlight",function(c){ return c == d.Name2;});
+            //tip.show(d);
+            d3.select("#tooltip").style({
+                visibility: "visible",
+                top: d3.event.clientY + "px",
+                left: d3.event.clientX + "px",
+                opacity: 1
+            });
+            d3.select("#tooltip").html("From: " + d.Name1 + "<br/>" +  "To: " + d.Name2 + "<br/>" + "Volume: " + d.Volume );
         })
         .on("mouseout", function (d) {
             d3.select(this).classed("cell-hover",false);
             d3.selectAll(".xLabel").classed("text-highlight",false);
             d3.selectAll(".yLabel").classed("text-highlight",false);
-            tip.hide(d);
+            //tip.hide(d);
+            d3.select("#tooltip").style({
+                visibility: "hidden",
+                opacity: 0
+            });
         });
     cells.exit().remove();
     cells.transition()
         .attr("x", function (d) {
         //return xElementsArray.indexOf(d.Name1) * heatmapCellSize;
         return yScale(d.Name1);
-    })
+        })
         .attr("y", function (d) {
             //return yElementsArray.indexOf(d.Name2) * heatmapCellSize;
             return yScale(d.Name2);
         })
 
         .attr("fill", function (d) {
-            return colorScale(d.Volume);
+            return heatmapColorScale(d.Volume);
         });
 
+    heatmapSvg.selectAll("g.heatmapAxis").remove();
     var xLabels = heatmapSvg.append("g")
         .attr("class", "x heatmapAxis")
         .call(xAxis)
@@ -197,8 +193,12 @@ function createHeatmap(data, data1) {
         .attr("dy", ".2em")
         .attr("class", function (d,i) { return "xLabel mono x"+ i;} )
         .style("text-anchor", "start")
-        .on("mouseover", function(d) {d3.select(this).classed("text-hover",true);})
-        .on("mouseout" , function(d) {d3.select(this).classed("text-hover",false);});
+        .on("mouseover", function(d) {
+            d3.select(this).classed("text-hover",true);
+        })
+        .on("mouseout" , function(d) {
+            d3.select(this).classed("text-hover",false);
+        });
     /*var xLabels = heatmapSvg.append("g")
             .selectAll(".xLabelg")
             .data(xElementsArray)
@@ -220,9 +220,15 @@ function createHeatmap(data, data1) {
         .attr("class", "y heatmapAxis")
         .call(yAxis)
         .selectAll("text")
-        .attr("class", function (d,i) { return "yLabel mono y"+ i;} )
-        .on("mouseover", function(d) {d3.select(this).classed("text-hover",true);})
-        .on("mouseout" , function(d) {d3.select(this).classed("text-hover",false);});
+        .attr("class", function (d,i) {
+            return "yLabel mono y"+ i;
+        })
+        .on("mouseover", function(d) {
+            d3.select(this).classed("text-hover",true);
+        })
+        .on("mouseout" , function(d) {
+            d3.select(this).classed("text-hover",false);
+        });
    /* var yLabels = heatmapSvg.append("g")
         .selectAll(".yLabelg")
         .data(yElementsArray)
@@ -238,11 +244,11 @@ function createHeatmap(data, data1) {
         .on("mouseover", function(d) {d3.select(this).classed("text-hover",true);})
         .on("mouseout" , function(d) {d3.select(this).classed("text-hover",false);});*/
 
-    //legendBar(colorScale);
+    //legendBar(heatmapColorScale);
 }
 
-/*function legendBar(colorScale) {
-    var legend = heatmapSvg.selectAll(".legend").data([0].concat(colorScale.quantiles()), function (d) {
+/*function legendBar(heatmapColorScale) {
+    var legend = heatmapSvg.selectAll(".legend").data([0].concat(heatmapColorScale.quantiles()), function (d) {
         return d;
     });
     legend.enter().append("g").attr("class", "legend");
@@ -268,20 +274,12 @@ function createHeatmap(data, data1) {
     legend.exit().remove();
 }*/
 
-/*function changeName(data) {
-    data.forEach(function (d) {
-        d.name1 = nameTrans(d.name1);
-        d.name2 = nameTrans(d.name2);
-    });
-    return data;
-}*/
-
 function nameTrans(email) {
     var nameString = email.substring(0,email.indexOf("@")).replace("."," ");
     var space = nameString.indexOf(" ");
-    var nameStringFrist = nameString.substring(0, space);
+    var nameStringFirst = nameString.substring(0, space);
     var nameStringLast = nameString.substring(space + 1);
-    return nameStringFrist.charAt(0).toUpperCase() + nameStringFrist.substring(1) + " " + nameStringLast.charAt(0).toUpperCase() + nameStringLast.substring(1);
+    return nameStringFirst.charAt(0).toUpperCase() + nameStringFirst.substring(1) + " " + nameStringLast.charAt(0).toUpperCase() + nameStringLast.substring(1);
 }
 
 
